ef6dd963bbcf4e12a1e699ad1f57d1d9eed16174
Nick <nick.brannies@gmail.com>
2019-11-10 20:17:26 -0800

    init

---
+++ PID Ball Balancer CONTROLLER/src/main.cpp
@@ -0,0 +1,151 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <WiFi101.h>
+
+#define BLYNK_PRINT SerialUSB
+#include <Blynk.h>
+//#include <BlynkSimpleWiFiShield101.h> //uncomment for wifi connection
+#include <BlynkSimpleSerialBLE.h> //uncomment for BLE connection
+
+///////////////////////Blynk Variables//////////////////////
+char auth[] = "zQR0v3kaPt-5QaGTjO-eNckoIXq82dEp";
+bool kp_manual = 0,ki_manual=0,kd_manual=0;
+float KP_MAN, KI_MAN, KD_MAN; //manual values for PID constants
+BlynkTimer timer;
+////////////////////////////////////////////////////////////
+
+/////////////////////WifiSetup//////////////////////////////
+char ssid[]="NETGEAR39"; //Wifi name
+char pass[] = "yellowsea022"; //Wifi password
+int status = WL_IDLE_STATUS; //connection status
+WiFiClient client;
+////////////////////////////////////////////////////////////
+
+///////////////////////Inputs/outputs///////////////////////
+int Analog_in = A0;
+Servo myservo;  // create servo object to control a servo, later attatched to D9
+///////////////////////////////////////////////////////
+
+
+////////////////////////Variables///////////////////////
+int Read = 0;
+float distance = 0.0,voltage,adc;
+float elapsedTime, time, timePrev;        //Variables for time control
+float distance_previous_error, distance_error;
+int period = 50;  //Refresh rate period of the loop is 50ms
+///////////////////////////////////////////////////////
+
+//////////////Communication with Other arduino///////////
+int voltage_out_pin = A1;
+int distance_out_pin = A2;
+/////////////////////////////////////////////////////////
+
+///////////////////PID constants///////////////////////
+float kp=1.5;
+float ki = 0.2;
+float kd=3100;
+float distance_setpoint = 20;    //distance to set ball from sensor
+float PID_p, PID_i, PID_d, PID_total;
+float startLoop, endLoop;
+///////////////////////////////////////////////////////
+
+//Function prototypes
+float map_to_float(float x, float a, float b, float c, float d);
+float get_dist(int n);
+
+void setup() { 
+  Serial1.begin(9600); //Setup serial stream for BLE connection through Serial1 (13,14)[Rx,Tx]
+  SerialUSB.begin(9600); //setup serial stream for USB debugging
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.write(125); //Put the servco at angle 125, so the balance is in the middle
+  pinMode(Analog_in,INPUT);  //Define input pin as an input for servo input
+  time = millis(); //Initialize current time
+
+  //Blynk stuff
+  //Blynk.begin(auth,ssid,pass); //uncomment for wifi connection
+  Blynk.begin(Serial1,auth); //uncomment for BLE connection
+}
+
+void loop() {
+  Blynk.run(); //start blynk checking for updates
+  timer.run(); //start blynk checking for interrupts
+
+  if (millis() > time+period){ //only loop if a period of time has passed
+    time = millis(); //reset begin time for each loop    
+    distance = get_dist(50);   
+    distance_error = distance_setpoint - distance;   
+    PID_p = (kp_manual ? KP_MAN : kp) * distance_error; //proportional correction
+    float dist_difference = distance_error - distance_previous_error;
+    PID_d = (kd_manual ? KD_MAN : kd)*((dist_difference)/period); //differential correction
+
+    //integral correction only works when close to target as this is only for small adjustments
+    if(-3 < distance_error && distance_error < 3){
+      PID_i = PID_i + ((ki_manual ? KI_MAN : ki ) * distance_error);
+    }
+    else{
+      PID_i = 0;
+    }
+
+    PID_total = PID_p + PID_i + PID_d; //Sum up total correction
+    PID_total = map(PID_total, -150, 150, 0, 150); //Map PID correction to an angle on the servo
+  
+    if(PID_total < 20){PID_total = 20;}
+    if(PID_total > 160) {PID_total = 160; } 
+  
+    myservo.write(PID_total+0); //PID total correction plus a correction constant
+    distance_previous_error = distance_error;
+
+  }
+}
+
+float map_to_float(float x, float a, float b, float c, float d){
+      float f=x/(b-a)*(d-c)+c;
+      return f;
+}
+
+float get_dist(int n){
+  long sum=0;
+  for(int i=0;i<n;i++)
+  {
+    sum=sum+analogRead(Analog_in);
+  }  
+  adc=sum/n;
+  
+  voltage = adc*3.3/1024;
+  float distance_cm = 5461/(adc-17)-2; //linear fit to trend of ADC value vs distance
+  if(distance_cm > 80) distance_cm = 81;
+  if(distance_cm < 10) distance_cm = 9;
+
+  //Debugging lines for sensor accuracy
+  String outputValues = String("Start,"+String(kp_manual ? KP_MAN : kp )+','+String(ki_manual ? KI_MAN : ki)+','+String(kd_manual ? KD_MAN : kd )+','+String(voltage)+','+String(adc)+','+String(distance_cm)+','+String(millis()));
+  SerialUSB.println(outputValues);
+
+  return(distance_cm);
+}
+
+//Blynk interupts to change values
+BLYNK_WRITE(V0){
+  if(kp_manual){
+  KP_MAN = param.asFloat();// assigning incoming value from pin V1 to a variable
+  } 
+}
+BLYNK_WRITE(V1){
+  if(kd_manual){
+  KD_MAN = param.asFloat(); // assigning incoming value from pin V1 to a variable
+  }
+}
+BLYNK_WRITE(V2){
+  if(ki_manual){
+  KI_MAN = param.asFloat(); // assigning incoming value from pin V1 to a variable
+  }
+}
+BLYNK_WRITE(V3){
+  kp_manual = param.asInt(); //toggle variable for manual override of Kp
+}
+BLYNK_WRITE(V4){
+  ki_manual = param.asInt(); //toggle variable for manual override of Ki
+}
+BLYNK_WRITE(V5){
+  kd_manual = param.asInt(); //toggle variable for manual override of Kd
+}
---
+++ Python Serial Plotter/PID_Plot.py
@@ -0,0 +1,115 @@
+import serial
+import serial.tools.list_ports
+import warnings
+import matplotlib.pyplot as plt
+from drawnow import *
+import atexit
+import time
+import sys
+
+#plotting values
+voltageValues = []
+adcValues = []
+distanceValues = []
+kpValues = []
+kiValues = []
+kdValues = []
+timeValues = []
+
+#serial variables
+serialCheckStr = ['Arduino','CH340'] #predefined list of possible arduino device names
+arduino_ports = []
+
+
+plt.ion()
+maxTime = 10 #x axis time in seconds
+period = 50 #period in miliseconds
+pointCount = 10/(period/1000) #calculate max time in seconds to run
+
+#find and connect to arduino serial port
+for p in serial.tools.list_ports.comports():
+    p.device
+    if any(x in p.description for x in serialCheckStr): #check for arduino ports using predefined list
+        arduino_ports.append(p.device)
+print(arduino_ports)
+if not arduino_ports:
+    raise IOError("No Arduino found")
+if len(arduino_ports) > 1:
+    warnings.warn('Multiple Arduinos found - using the first')
+serialArduino = serial.Serial(arduino_ports[0])  #9600 default baud rate
+
+#initialize first connections
+def plotValues():
+    plt.title('Distance vs Time')
+    plt.grid(True)
+    plt.ylabel('Distance [cm]')
+    plt.xlabel('Time [s]')
+    plt.plot(timeValues,distanceValues)
+    plt.legend("Distance",loc='upper right')
+
+def doAtExit():
+    serialArduino.close()
+    print("Close serial")
+    print("serialArduino.isOpen() = " + str(serialArduino.isOpen()))
+
+def addPoints():
+    pass
+
+atexit.register(doAtExit)
+
+print("serialArduino.isOpen() = " + str(serialArduino.isOpen()))
+
+#pre-load dummy data
+for i in range(0,int(pointCount)):
+    voltageValues.append(0)
+    adcValues.append(0)
+    distanceValues.append(0)
+    kpValues.append(0)
+    kiValues.append(0)
+    kdValues.append(0)
+    timeValues.append(0)
+
+while True:
+    start = time.time()
+    
+    while (serialArduino.inWaiting()==0):
+        pass
+    serialLine = serialArduino.readline(500).strip()
+    serialLine = serialLine.decode("utf-8") #decode bytes into string value and strip \r\n off
+    serialValues = serialLine.split(',')
+    print(serialValues[0])
+    #only run loop if the first element is indicating the start of a data row
+    if serialValues[0] == 'Start':
+        serialValues.pop(0) #remove 'Start' from data since it is only an indicator
+        print(serialValues)
+        try:
+            #process string data and assign to variables to plot
+            adcValue = float(serialValues[4])
+            voltage = float(serialValues[3])
+            distance_cm = float(serialValues[5])
+            millis = int(serialValues[6]) #current run time for arduino modded with max time
+
+            ## add values to array to plot
+
+            #add new values
+            distanceValues.append(distance_cm)
+            timeValues.append(millis/1000)
+
+            #remove old values
+            distanceValues.pop(0)
+            timeValues.pop(0)
+
+            #plot new data
+            drawnow(plotValues)
+        except ValueError:
+            print("Invalid! cannot cast")
+        end = time.time()
+        executeTime_ms = (end - start)*1000
+        
+        #clear buffer of serial inputs, to allow python to go as fast as it can
+        #This allows for the program to not 'lag' behind arduino output
+        #Unfortunately this does reduce the amount of accuracy present by the graph
+        #depending on the speed of this python program
+        serialArduino.flushInput() 
+        print("Execute time:", round(executeTime_ms,1),"Waiting:",serialArduino.inWaiting())
+    
